ce5f68355897ca924d8846ec9790557aaf6c8d87
sujay <jirpuresujay@gmail.com>
2025-12-27 23:29:39 +0530

    changes 6

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.send("Dashboard backend running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
