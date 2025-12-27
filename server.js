845ec9a520f75ff1377d4d107b4818c0c628312c
sujay <jirpuresujay@gmail.com>
2025-12-27 23:22:13 +0530

    changes 5

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import inquiryRoutes from "./routes/inquiry.js";
+import inquiryRoutes from "./routes/Inquiry.js";
 
 dotenv.config();
 
